fd9e40f99f6ef6a7c1e9e69db93a94a75387c9d1
zzzyoonnn <dwsrh03@gmail.com>
2025-02-11 17:48:05 +0900

    feat: redirect 기능을 하는 sendRedirect 구현

--- src/main/java/org/servlet2spring/servlet2spring/todo/TodoRegisterController.java
+++ src/main/java/org/servlet2spring/servlet2spring/todo/TodoRegisterController.java
@@ -23,5 +23,7 @@ public class TodoRegisterController extends HttpServlet {
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     System.out.println("입력을 처리하고 목록 페이지로 이동");
+
+    resp.sendRedirect("/todo/list");
   }
 }
