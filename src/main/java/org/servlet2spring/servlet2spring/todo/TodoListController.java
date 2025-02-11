06793862f5a30270c1fa3742e31401ebb3b4fcb8
zzzyoonnn <dwsrh03@gmail.com>
2025-02-11 17:24:34 +0900

    feat: 목록 기능을 하는 TodoListController 구현

---
+++ src/main/java/org/servlet2spring/servlet2spring/todo/TodoListController.java
@@ -0,0 +1,19 @@
+package org.servlet2spring.servlet2spring.todo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "todoListController", urlPatterns = "/todo/list")
+public class TodoListController extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    System.out.println("/todo/list");
+
+    req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req, resp);
+  }
+}
---
+++ src/main/webapp/WEB-INF/todo/list.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jiyoon
+  Date: 2/11/25
+  Time: 5:21 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>List Page</title>
+</head>
+<body>
+<h1>List Page</h1>
+</body>
+</html>
