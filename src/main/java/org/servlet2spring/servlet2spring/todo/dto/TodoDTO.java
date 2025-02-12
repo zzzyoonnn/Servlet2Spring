f5f4b8daa531c5669a5cbb30ea0b7b5d46994f0f
zzzyoonnn <dwsrh03@gmail.com>
2025-02-12 23:08:59 +0900

    feat: DTO를 위한 클래스 TodoDTO 구현

---
+++ src/main/java/org/servlet2spring/servlet2spring/todo/dto/TodoDTO.java
@@ -0,0 +1,47 @@
+package org.servlet2spring.servlet2spring.todo.dto;
+
+import java.time.LocalDate;
+
+public class TodoDTO {
+  private Long no;
+  private String title;
+  private LocalDate dueDate;
+  private boolean finished;
+
+  public Long getNo() {
+    return no;
+  }
+
+  public void setNo(Long no) {
+    this.no = no;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public LocalDate getDueDate() {
+    return dueDate;
+  }
+
+  public void setDueDate(LocalDate dueDate) {
+    this.dueDate = dueDate;
+  }
+
+  public boolean isFinished() {
+    return finished;
+  }
+
+  public void setFinished(boolean finished) {
+    this.finished = finished;
+  }
+
+  @Override
+  public String toString() {
+    return "TodoDTO [no=" + no + ", title='" + title + + '\'' + ", dueDate=" + dueDate + ", finished=" + finished + "]";
+  }
+}
