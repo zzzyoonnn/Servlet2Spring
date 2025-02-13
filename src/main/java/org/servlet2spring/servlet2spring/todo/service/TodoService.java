5f8d0cd3a4161e7a4cead6839bd47ee915ce2293
zzzyoonnn <dwsrh03@gmail.com>
2025-02-13 17:12:06 +0900

    feat: List<TodoDTO>를 확인하기 위한 JSP 구현

--- src/main/java/org/servlet2spring/servlet2spring/todo/service/TodoService.java
+++ src/main/java/org/servlet2spring/servlet2spring/todo/service/TodoService.java
@@ -1,6 +1,7 @@
 package org.servlet2spring.servlet2spring.todo.service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -9,10 +10,14 @@ import org.servlet2spring.servlet2spring.todo.dto.TodoDTO;
 public enum TodoService {
   INSTANCE;
 
+  private final List<TodoDTO> todoList = new ArrayList<>();
+
+  // 새로운 TodoDTO 객체를 받아서 확인 가능
   public void register(TodoDTO todoDTO) {
     System.out.println("DEBUG... " + todoDTO);
   }
 
+  // 10개의 TodoDTO 객체를 만들어서 반환
   public List<TodoDTO> getList() {
     return IntStream.range(0, 10).mapToObj(i -> {
       TodoDTO dto = new TodoDTO();
--- src/main/webapp/WEB-INF/todo/list.jsp
+++ src/main/webapp/WEB-INF/todo/list.jsp
@@ -12,5 +12,8 @@
 </head>
 <body>
 <h1>List Page</h1>
+
+${todoList}
+
 </body>
 </html>
