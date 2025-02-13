bf6d8a30b012138bc0d9a61a1fe2ef87412ca02a
zzzyoonnn <dwsrh03@gmail.com>
2025-02-13 16:42:36 +0900

    feat: TodoService로부터 List<TodoDTO>를 JSP로 전달하는 기능 구현

--- src/main/java/org/servlet2spring/servlet2spring/todo/TodoListController.java
+++ src/main/java/org/servlet2spring/servlet2spring/todo/TodoListController.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import org.servlet2spring.servlet2spring.todo.dto.TodoDTO;
+import org.servlet2spring.servlet2spring.todo.service.TodoService;
 
 @WebServlet(name = "todoListController", urlPatterns = "/todo/list")
 public class TodoListController extends HttpServlet {
@@ -13,6 +16,10 @@ public class TodoListController extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     System.out.println("/todo/list");
+
+    List<TodoDTO> dtoList = TodoService.INSTANCE.getList();
+
+    req.setAttribute("todoList", dtoList);
 
     req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req, resp);
   }
