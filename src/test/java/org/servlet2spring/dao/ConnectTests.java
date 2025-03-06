55a737b976e371c164176674f9c1febabe64a89a
zzzyoonnn <dwsrh03@gmail.com>
2025-03-06 17:03:41 +0900

    test: JDBC 실행 가능 여부 테스트 구현

---
+++ src/test/java/org/servlet2spring/dao/ConnectTests.java
@@ -0,0 +1,15 @@
+package org.servlet2spring.dao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConnectTests {
+
+  @Test
+  public void test() {
+    int v1 = 10;
+    int v2 = 10;
+
+    Assertions.assertEquals(v1, v2);
+  }
+}
