244dd814e5b88af91b6c3c839d4248936f82f5ce
zzzyoonnn <dwsrh03@gmail.com>
2025-03-07 22:56:25 +0900

    test: 자바 코드와 MySQL 연결 확인 테스트 구현

--- src/test/java/org/servlet2spring/dao/ConnectTests.java
+++ src/test/java/org/servlet2spring/dao/ConnectTests.java
@@ -1,9 +1,27 @@
 package org.servlet2spring.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ConnectTests {
+
+  @Test
+  public void testConnection() throws Exception {
+    Class.forName("com.mysql.cj.jdbc.Driver");
+
+    Connection connection = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/s2s",
+            "s2s",
+            "s2s"
+    );
+
+    Assertions.assertNotNull(connection);
+
+    connection.close();
+
+  }
 
   @Test
   public void test() {
