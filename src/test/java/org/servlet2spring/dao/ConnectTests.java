7edd04f2c98c85b2bcf5bb73a1e75e53fd60e7e8
zzzyoonnn <dwsrh03@gmail.com>
2025-03-12 23:24:12 +0900

    test: HikariCP를 이용하는 테스트 구현

--- src/test/java/org/servlet2spring/dao/ConnectTests.java
+++ src/test/java/org/servlet2spring/dao/ConnectTests.java
@@ -1,11 +1,33 @@
 package org.servlet2spring.dao;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ConnectTests {
+
+  @Test
+  public void testHikariCP() throws Exception {
+    HikariConfig config = new HikariConfig();
+    config.setDriverClassName("com.mysql.jdbc.Driver");
+    config.setJdbcUrl("jdbc:mysql://localhost:3306/s2s");
+    config.setUsername("s2s");
+    config.setPassword("s2s");
+    config.addDataSourceProperty("cachePrepStmts", "true");
+    config.addDataSourceProperty("prepStmtCacheSize", "250");
+    config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+    HikariDataSource ds = new HikariDataSource(config);
+    Connection conn = ds.getConnection();
+
+    System.out.println(conn);
+
+    conn.close();
+  }
+
 
   @Test
   public void testConnection() throws Exception {
