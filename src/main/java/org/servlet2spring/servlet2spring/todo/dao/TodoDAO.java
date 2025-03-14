51fc9ff0d6c68899084a97cf6989e4193fcda67e
zzzyoonnn <dwsrh03@gmail.com>
2025-03-14 16:22:24 +0900

    feat: tbl_todo 내의 모든 데이터를 가져오는 selectAll() 메서드 구현

--- src/main/java/org/servlet2spring/servlet2spring/todo/dao/TodoDAO.java
+++ src/main/java/org/servlet2spring/servlet2spring/todo/dao/TodoDAO.java
@@ -4,10 +4,35 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Cleanup;
 import org.servlet2spring.servlet2spring.todo.domain.TodoVO;
 
 public class TodoDAO {
+
+  public List<TodoVO> selectAll() throws Exception {
+    String sql = "select * from tbl_todo";
+
+    @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+    @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(sql);
+    @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+    List<TodoVO> list = new ArrayList<>();
+
+    while (resultSet.next()) {
+      TodoVO vo = TodoVO.builder()
+              .no(resultSet.getLong("no"))
+              .title(resultSet.getString("title"))
+              .dueDate(resultSet.getDate("dueDate").toLocalDate())
+              .finished(resultSet.getBoolean("finished"))
+              .build();
+
+      list.add(vo);
+    }
+
+    return list;
+  }
 
   public void insert(TodoVO vo) throws Exception {
     String sql = "insert into tbl_todo (title, dueDate, finished) values (?, ?, ?)";
