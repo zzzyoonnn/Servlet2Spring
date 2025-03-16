dff96cd72d3895ab2951b2a9db9bdae3278d08ac
zzzyoonnn <dwsrh03@gmail.com>
2025-03-16 14:34:19 +0900

    feat: ModelMapper를 이용하여 TodoDTO -> TodoVO 매핑 기능 구현

--- src/main/java/org/servlet2spring/servlet2spring/todo/service/TodoService.java
+++ src/main/java/org/servlet2spring/servlet2spring/todo/service/TodoService.java
@@ -1,42 +1,28 @@
 package org.servlet2spring.servlet2spring.todo.service;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import org.modelmapper.ModelMapper;
+import org.servlet2spring.servlet2spring.todo.dao.TodoDAO;
+import org.servlet2spring.servlet2spring.todo.domain.TodoVO;
 import org.servlet2spring.servlet2spring.todo.dto.TodoDTO;
+import org.servlet2spring.servlet2spring.todo.util.MapperUtil;
 
 public enum TodoService {
   INSTANCE;
 
-  private final List<TodoDTO> todoList = new ArrayList<>();
+  private TodoDAO todoDAO;
+  private ModelMapper modelMapper;
 
-  // 새로운 TodoDTO 객체를 받아서 확인 가능
-  public void register(TodoDTO todoDTO) {
-    System.out.println("DEBUG... " + todoDTO);
+  TodoService() {
+    todoDAO = new TodoDAO();
+    modelMapper = MapperUtil.INSTANCE.getModelMapper();
   }
 
-  // 특정한 번호 조회 기능
-  public TodoDTO get(Long no) {
-    TodoDTO todoDto = new TodoDTO();
-    todoDto.setNo(no);
-    todoDto.setTitle("Sample Todo");
-    todoDto.setDueDate(LocalDate.now());
-    todoDto.setFinished(true);
+  public void register(TodoDTO todoDTO) throws Exception {
+    TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
 
-    return todoDto;
+    System.out.println("todoVO: " + todoVO);
+
+    todoDAO.insert(todoVO);
   }
 
-  // 10개의 TodoDTO 객체를 만들어서 반환
-  public List<TodoDTO> getList() {
-    return IntStream.range(0, 10).mapToObj(i -> {
-      TodoDTO dto = new TodoDTO();
-      dto.setNo((long)i);
-      dto.setTitle("Todo : " + i);
-      dto.setDueDate(LocalDate.now());
-
-      return dto;
-    }).collect(Collectors.toList());
-  }
 }
