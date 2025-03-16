a6239cffe8614d3a0e28d6acb108e6dd0a8782be
zzzyoonnn <dwsrh03@gmail.com>
2025-03-16 13:38:52 +0900

    refactor: TodoDTO에 Lombok 적용하여 코드 개선

--- src/main/java/org/servlet2spring/servlet2spring/todo/dto/TodoDTO.java
+++ src/main/java/org/servlet2spring/servlet2spring/todo/dto/TodoDTO.java
@@ -1,47 +1,23 @@
 package org.servlet2spring.servlet2spring.todo.dto;
 
 import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TodoDTO {
+
   private Long no;
+
   private String title;
+
   private LocalDate dueDate;
+
   private boolean finished;
 
-  public Long getNo() {
-    return no;
-  }
-
-  public void setNo(Long no) {
-    this.no = no;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public LocalDate getDueDate() {
-    return dueDate;
-  }
-
-  public void setDueDate(LocalDate dueDate) {
-    this.dueDate = dueDate;
-  }
-
-  public boolean isFinished() {
-    return finished;
-  }
-
-  public void setFinished(boolean finished) {
-    this.finished = finished;
-  }
-
-  @Override
-  public String toString() {
-    return "TodoDTO [no=" + no + ", title='" + title + '\'' + ", dueDate=" + dueDate + ", finished=" + finished + "]";
-  }
 }
