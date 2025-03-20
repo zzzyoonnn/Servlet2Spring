0e862cc4e8191475b68b6d78cc85ccfa1c4eecea
zzzyoonnn <dwsrh03@gmail.com>
2025-03-21 00:43:01 +0900

    test: TodoServiceTests의 Log4j2 적용 테스트 구현

--- src/test/java/org/servlet2spring/service/TodoServiceTests.java
+++ src/test/java/org/servlet2spring/service/TodoServiceTests.java
@@ -1,11 +1,13 @@
 package org.servlet2spring.service;
 
 import java.time.LocalDate;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.servlet2spring.servlet2spring.todo.dto.TodoDTO;
 import org.servlet2spring.servlet2spring.todo.service.TodoService;
 
+@Log4j2
 public class TodoServiceTests {
 
   private TodoService todoService;
@@ -18,9 +20,12 @@ public class TodoServiceTests {
   @Test
   public void testRegister() throws Exception {
     TodoDTO todoDTO = TodoDTO.builder()
-            .title("JDBC Test(TodoService)")
+            .title("JDBC Log4j2 Test(TodoService)")
             .dueDate(LocalDate.now())
             .build();
+
+    log.info("----------테스트 코드의 Log4j2 설정 확인----------");
+    log.info(todoDTO);
 
     todoService.register(todoDTO);
   }
