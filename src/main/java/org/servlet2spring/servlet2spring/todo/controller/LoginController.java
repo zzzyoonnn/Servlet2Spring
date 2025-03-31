192e78ca306fe5c0eeed77f290e9408e9a3db8a4
zzzyoonnn <dwsrh03@gmail.com>
2025-03-31 16:25:53 +0900

    feat: 입력받은 id/pw을 이용하여 로그인하는 기능 구현

--- src/main/java/org/servlet2spring/servlet2spring/todo/controller/LoginController.java
+++ src/main/java/org/servlet2spring/servlet2spring/todo/controller/LoginController.java
@@ -5,12 +5,28 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import lombok.extern.java.Log;
 
 @Log
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    log.info("login post...");
+
+    String mid = req.getParameter("mid");
+    String mpw = req.getParameter("mpw");
+
+    String str = mid+mpw;
+
+    HttpSession session = req.getSession();
+    session.setAttribute("loginInfo", str);
+
+    resp.sendRedirect("/todo/list");
+  }
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
