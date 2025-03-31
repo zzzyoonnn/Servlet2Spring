e9de45d3312c69ab30b7cda1b73bdd9c4510692f
zzzyoonnn <dwsrh03@gmail.com>
2025-03-31 15:43:58 +0900

    feat: 로그인한 사용자만이 '/register'에 접근 가능하도록 세션을 확인하는 기능 구현

--- src/main/java/org/servlet2spring/servlet2spring/todo/controller/TodoRegisterController.java
+++ src/main/java/org/servlet2spring/servlet2spring/todo/controller/TodoRegisterController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -22,6 +23,23 @@ public class TodoRegisterController extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     log.info("/todo/register GET...");
+
+    HttpSession session = req.getSession();
+
+    if (session.isNew()) {
+      log.info("JSESSIONID 쿠키가 새로 만들어진 사용자: " + session.getId());
+      resp.sendRedirect("/login");
+      return;
+    }
+
+    // JSESSIONID는 있지만 해당 세션 컨텍스트에 loginInfo라는 이름으로 저장된 객체가 없는 경우
+    if (session.getAttribute("logInfo") == null) {
+      log.info("로그인한 정보가 없는 사용자");
+      resp.sendRedirect("/login");
+      return;
+    }
+
+    // 정상적인 경우
     req.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(req, resp);
   }
 
