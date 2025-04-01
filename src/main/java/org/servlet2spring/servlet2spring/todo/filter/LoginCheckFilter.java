e727b56caef58e9aaca932c4b8aecbe2ac2c3d54
zzzyoonnn <dwsrh03@gmail.com>
2025-04-01 15:12:11 +0900

    feat: 로그인 여부 확인 기능 구현

--- src/main/java/org/servlet2spring/servlet2spring/todo/filter/LoginCheckFilter.java
+++ src/main/java/org/servlet2spring/servlet2spring/todo/filter/LoginCheckFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import lombok.extern.log4j.Log4j2;
 
@@ -16,6 +19,17 @@ public class LoginCheckFilter implements Filter {
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
     log.info("Login check filter...");
+
+    HttpServletRequest req = (HttpServletRequest) request;
+    HttpServletResponse resp = (HttpServletResponse) response;
+
+    HttpSession session = req.getSession();
+
+    if (session.getAttribute("loginInfo") == null) {
+      resp.sendRedirect("/login");
+
+      return;
+    }
 
     chain.doFilter(request, response);
   }
