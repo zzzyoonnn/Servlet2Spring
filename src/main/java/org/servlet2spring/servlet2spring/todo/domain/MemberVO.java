fbcc2d8c6a5bd9bd5cff0dc7d1adef02d546ab63
zzzyoonnn <dwsrh03@gmail.com>
2025-04-03 14:36:23 +0900

    feat: 데이터베이스 member 테이블의 한 행을 객체로 표현하는 MemberVO 구현

---
+++ src/main/java/org/servlet2spring/servlet2spring/todo/domain/MemberVO.java
@@ -0,0 +1,19 @@
+package org.servlet2spring.servlet2spring.todo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberVO {
+
+  private String mid;
+  private String mpw;
+  private String mname;
+}
