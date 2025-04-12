7c49cf34526871348b3f4d0b5fe0fe39bd23d09b
zzzyoonnn <dwsrh03@gmail.com>
2025-04-12 16:29:27 +0900

    feat: uuid 값으로 사용자를 찾는 기능 구현

--- src/main/java/org/servlet2spring/servlet2spring/todo/service/MemberService.java
+++ src/main/java/org/servlet2spring/servlet2spring/todo/service/MemberService.java
@@ -18,7 +18,18 @@ public enum MemberService {
     memberDAO = new MemberDAO();
     modelMapper = MapperUtil.INSTANCE.getModelMapper();
   }
-  
+
+  // uuid 값으로 사용자를 찾는 기능
+  public MemberDTO getByUUID(String uuid) throws Exception {
+
+    MemberVO memberVO = memberDAO.selectUUID(uuid);
+
+    MemberDTO memberDTO = modelMapper.map(memberVO, MemberDTO.class);
+
+    return memberDTO;
+  }
+
+
   // 자동 로그인 기능
   public void updateUuid(String mid, String uuid) throws Exception {
 
