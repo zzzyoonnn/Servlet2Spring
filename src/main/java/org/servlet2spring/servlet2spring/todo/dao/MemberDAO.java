2a9a0ef99b749efab65876e5d83e3735875c56d8
zzzyoonnn <dwsrh03@gmail.com>
2025-04-12 16:26:23 +0900

    feat: 쿠키를 이용하여 사용자 정보를 로딩하는 기능 구현

--- src/main/java/org/servlet2spring/servlet2spring/todo/dao/MemberDAO.java
+++ src/main/java/org/servlet2spring/servlet2spring/todo/dao/MemberDAO.java
@@ -8,6 +8,28 @@ import org.servlet2spring.servlet2spring.todo.domain.MemberVO;
 
 public class MemberDAO {
 
+  // 쿠키를 이용해서 해당 사용자 정보 로딩 기능
+  public MemberVO selectUUID(String uuid) throws Exception {
+    String query = "select mid, mpw, mname, uuid from tbl_member where uuid = ?";
+
+    @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+    @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(query);
+    preparedStatement.setString(1, uuid);
+
+    @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+    resultSet.next();
+
+    MemberVO memberVO = MemberVO.builder()
+            .mid(resultSet.getString(1))
+            .mpw(resultSet.getString(2))
+            .mname(resultSet.getString(3))
+            .uuid(resultSet.getString(4))
+            .build();
+
+    return memberVO;
+  }
+
   // 자동 로그인 기능
   public void updateUuid(String mid, String uuid) throws Exception {
 
