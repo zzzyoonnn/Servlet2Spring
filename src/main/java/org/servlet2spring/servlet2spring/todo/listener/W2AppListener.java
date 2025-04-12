19fa5cbaf9c1c95cb1229f99be78605dd2ecbaeb
zzzyoonnn <dwsrh03@gmail.com>
2025-04-13 00:14:09 +0900

    feat: Listener 기능 구현

---
+++ src/main/java/org/servlet2spring/servlet2spring/todo/listener/W2AppListener.java
@@ -0,0 +1,27 @@
+package org.servlet2spring.servlet2spring.todo.listener;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@WebListener
+public class W2AppListener implements ServletContextListener {
+
+  @Override
+  public void contextInitialized(ServletContextEvent sce) {
+
+    log.info("---init---");
+    log.info("---init---");
+    log.info("---init---");
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent sce) {
+
+    log.info("---destroy---");
+    log.info("---destroy---");
+    log.info("---destroy---");
+  }
+}
