02006088d5a64852f578791951bb656003974bb0
zzzyoonnn <dwsrh03@gmail.com>
2025-04-28 15:37:08 +0900

    feat: @Autowired를 사용해서 SampleDAO 주입

--- src/main/java/org/servlet2spring/servlet2spring/springSample/SampleDAO.java
+++ src/main/java/org/servlet2spring/servlet2spring/springSample/SampleDAO.java
@@ -1,4 +1,11 @@
 package org.servlet2spring.servlet2spring.springSample;
 
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ToString
 public class SampleDAO {
+
+  @Autowired
+  private SampleDAO sampleDAO;
 }
