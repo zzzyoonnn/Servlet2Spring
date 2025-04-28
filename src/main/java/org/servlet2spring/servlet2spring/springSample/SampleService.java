2e4b476b9c490d2769fff7f8b9edf06d6cb992b2
zzzyoonnn <dwsrh03@gmail.com>
2025-04-28 17:13:26 +0900

    refactor: 생성자 주입 방식을 @RequiredArgsConstructor로 개선

--- src/main/java/org/servlet2spring/servlet2spring/springSample/SampleService.java
+++ src/main/java/org/servlet2spring/servlet2spring/springSample/SampleService.java
@@ -1,13 +1,15 @@
 package org.servlet2spring.servlet2spring.springSample;
 
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @ToString
+@RequiredArgsConstructor
 public class SampleService {
 
   @Autowired
-  private SampleDAO sampleDAO;
+  private final SampleDAO sampleDAO;
 }
