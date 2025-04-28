f853ddeb3a01b558121d7f73385c514808f3f5a3
zzzyoonnn <dwsrh03@gmail.com>
2025-04-28 17:11:16 +0900

    feat: 스프링에서 빈으로 관리될 수 있도록 어노테이션 추가

--- src/main/java/org/servlet2spring/servlet2spring/springSample/SampleService.java
+++ src/main/java/org/servlet2spring/servlet2spring/springSample/SampleService.java
@@ -2,7 +2,9 @@ package org.servlet2spring.servlet2spring.springSample;
 
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 @ToString
 public class SampleService {
 
