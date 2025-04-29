a3ac9d59af958d4419d3dd246e8eb7ec826a712a
zzzyoonnn <dwsrh03@gmail.com>
2025-04-29 15:37:06 +0900

    feat: SampleDAO의 구현체들에 @Qualifier 적용

--- src/main/java/org/servlet2spring/servlet2spring/springSample/SampleDAOImpl.java
+++ src/main/java/org/servlet2spring/servlet2spring/springSample/SampleDAOImpl.java
@@ -1,7 +1,9 @@
 package org.servlet2spring.servlet2spring.springSample;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Qualifier("normal")
 public class SampleDAOImpl implements SampleDAO {
 }
--- src/main/java/org/servlet2spring/servlet2spring/springSample/SampleEventDAOImpl.java
+++ src/main/java/org/servlet2spring/servlet2spring/springSample/SampleEventDAOImpl.java
@@ -1,9 +1,9 @@
 package org.servlet2spring.servlet2spring.springSample;
 
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
-@Primary
 @Repository
+@Qualifier("event")
 public class SampleEventDAOImpl implements SampleDAO{
 }
