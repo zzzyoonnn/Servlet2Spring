d66c23e115f86129c6124cef1ba579b0bed961fa
zzzyoonnn <dwsrh03@gmail.com>
2025-04-29 15:29:01 +0900

    fix: SampleDAO 구현체 충돌 해결을 위해 @Primary 적용

---
+++ src/main/java/org/servlet2spring/servlet2spring/springSample/SampleEventDAOImpl.java
@@ -0,0 +1,9 @@
+package org.servlet2spring.servlet2spring.springSample;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+@Primary
+@Repository
+public class SampleEventDAOImpl implements SampleDAO{
+}
