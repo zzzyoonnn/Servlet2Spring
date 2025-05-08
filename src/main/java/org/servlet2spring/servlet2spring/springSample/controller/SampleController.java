4370b1d71ab8f85c58cd3e95623dece8cfddb217
zzzyoonnn <dwsrh03@gmail.com>
2025-05-08 22:51:24 +0900

    feat: 스프링 MVC 컨트롤러를 실습 하기 위한 SampleController 생성

---
+++ src/main/java/org/servlet2spring/servlet2spring/springSample/controller/SampleController.java
@@ -0,0 +1,15 @@
+package org.servlet2spring.servlet2spring.springSample.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Log4j2
+@Controller
+public class SampleController {
+
+  @GetMapping("/hello")
+  public void hello() {
+    log.info("hello...");
+  }
+}
