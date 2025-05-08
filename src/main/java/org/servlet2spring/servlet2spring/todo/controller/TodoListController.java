4acba3844a1b4ed99daf6f63f25b767b298f7c4d
zzzyoonnn <dwsrh03@gmail.com>
2025-05-08 23:26:56 +0900

    refactor: 기존 Servlet 기반 controller를 Spring MVC 방식으로 마이그레이션

--- src/main/java/org/servlet2spring/servlet2spring/todo/controller/TodoListController.java
+++ src/main/java/org/servlet2spring/servlet2spring/todo/controller/TodoListController.java
@@ -12,27 +12,45 @@ import lombok.extern.log4j.Log4j2;
 import jakarta.servlet.annotation.WebServlet;
 import org.servlet2spring.servlet2spring.todo.dto.TodoDTO;
 import org.servlet2spring.servlet2spring.todo.service.TodoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Log4j2
-@WebServlet(name = "todoListController", value = "/todo/list")
+@Controller
+@RequestMapping("/todo")
+//@WebServlet(name = "todoListController", value = "/todo/list")
 public class TodoListController extends HttpServlet {
   private TodoService todoService = TodoService.INSTANCE;
 
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    log.info("todoList...");
-
-    ServletContext servletContext = req.getServletContext();
-
-    log.info("appName: " + servletContext.getAttribute("appName"));
-
-    try {
-      List<TodoDTO> dtoList = todoService.listAll();
-      req.setAttribute("dtoList", dtoList);
-      req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req, resp);
-    } catch (Exception e) {
-      log.error(e.getMessage());
-      throw new ServletException("list error");
-    }
+  @RequestMapping("/list")
+  public void list() {
+    log.info("todo list...");
   }
+
+  //@RequestMapping(value = "/register", method = RequestMethod.GET)
+  @GetMapping("/register")
+  public void registerGET() {
+    log.info("GET todo register...");
+  }
+
+
+//  @Override
+//  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//    log.info("todoList...");
+//
+//    ServletContext servletContext = req.getServletContext();
+//
+//    log.info("appName: " + servletContext.getAttribute("appName"));
+//
+//    try {
+//      List<TodoDTO> dtoList = todoService.listAll();
+//      req.setAttribute("dtoList", dtoList);
+//      req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req, resp);
+//    } catch (Exception e) {
+//      log.error(e.getMessage());
+//      throw new ServletException("list error");
+//    }
+//  }
 }
