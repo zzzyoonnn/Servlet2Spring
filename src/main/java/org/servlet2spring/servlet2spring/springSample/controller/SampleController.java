6b1b18f1bfd00b8c40d05d42402d90744f874a78
zzzyoonnn <dwsrh03@gmail.com>
2025-05-09 16:15:39 +0900

    feat: Model이라는 객체를 이용하는 파라미터 처리 실습

--- src/main/java/org/servlet2spring/servlet2spring/springSample/controller/SampleController.java
+++ src/main/java/org/servlet2spring/servlet2spring/springSample/controller/SampleController.java
@@ -2,8 +2,11 @@ package org.servlet2spring.servlet2spring.springSample.controller;
 
 import java.time.LocalDate;
 import lombok.extern.log4j.Log4j2;
+import org.servlet2spring.servlet2spring.todo.dto.TodoDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -29,5 +32,22 @@ public class SampleController {
   public void ex2(@RequestParam("dueDate") LocalDate dueDate) {
     log.info("ex2...");
     log.info("dueDate:" + dueDate);
+  }
+
+  @GetMapping("/ex3")
+  public void ex3(Model model) {
+    log.info("-----------------------------------");
+
+    model.addAttribute("message", "Hello World");
+  }
+
+  @GetMapping("/ex4")
+  public void ex4(TodoDTO todoDTO, Model model) {
+    log.info("ex4...");
+  }
+
+  @GetMapping("ex5")
+  public void ex5(@ModelAttribute("dto") TodoDTO todoDTO, Model model) {
+    log.info(todoDTO);
   }
 }
---
+++ src/main/webapp/WEB-INF/views/ex3.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jiyoon
+  Date: 5/9/25
+  Time: 3:38 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Model Parameter</title>
+</head>
+<body>
+<h1>${message}</h1>
+</body>
+</html>
