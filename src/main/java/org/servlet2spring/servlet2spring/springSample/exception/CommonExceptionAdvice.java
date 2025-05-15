e09505e50832a50f4f2ef6e47af8cd0c96c677e6
zzzyoonnn <dwsrh03@gmail.com>
2025-05-15 19:34:38 +0900

    feat: 예외 처리 범위를 넓히기 위해 Exception 타입 예외 처리 추가

--- src/main/java/org/servlet2spring/servlet2spring/springSample/exception/CommonExceptionAdvice.java
+++ src/main/java/org/servlet2spring/servlet2spring/springSample/exception/CommonExceptionAdvice.java
@@ -1,5 +1,6 @@
 package org.servlet2spring.servlet2spring.springSample.exception;
 
+import java.util.Arrays;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,6 +9,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Log4j2
 @ControllerAdvice
 public class CommonExceptionAdvice {
+
+  @ResponseBody
+  @ExceptionHandler(Exception.class)
+  public String exceptionCommon(Exception exception) {
+    log.error("---------------------------------");
+    log.error(exception.getMessage());
+
+    StringBuffer buffer = new StringBuffer("<ul>");
+
+    buffer.append("<li>" + exception.getMessage() + "</li>");
+
+    Arrays.stream(exception.getStackTrace()).forEach(stackTraceElement -> {
+      buffer.append("<li>" + stackTraceElement.toString() + "</li>");
+    });
+    buffer.append("</ul>");
+
+    return buffer.toString();
+  }
+
 
   @ResponseBody
   @ExceptionHandler(NumberFormatException.class)
