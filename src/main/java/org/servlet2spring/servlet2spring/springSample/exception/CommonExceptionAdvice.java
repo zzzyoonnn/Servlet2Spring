f5039baf4ca119b0fb9455aae0637fdd42e8fc50
zzzyoonnn <dwsrh03@gmail.com>
2025-05-15 18:47:53 +0900

    feat: NumberFormatException 발생 시 예외 처리 추가

--- src/main/java/org/servlet2spring/servlet2spring/springSample/exception/CommonExceptionAdvice.java
+++ src/main/java/org/servlet2spring/servlet2spring/springSample/exception/CommonExceptionAdvice.java
@@ -2,8 +2,19 @@ package org.servlet2spring.servlet2spring.springSample.exception;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Log4j2
 @ControllerAdvice
 public class CommonExceptionAdvice {
+
+  @ResponseBody
+  @ExceptionHandler(NumberFormatException.class)
+  public String exceptionHandler(NumberFormatException numberFormatException) {
+    log.error("---------------------------------");
+    log.error(numberFormatException.getMessage());
+
+    return "NUMBER FORMAT EXCEPTION";
+  }
 }
