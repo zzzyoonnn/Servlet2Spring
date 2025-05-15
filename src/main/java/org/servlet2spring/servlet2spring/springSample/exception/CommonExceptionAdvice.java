ab768e06d95a60dc4e3f590ac38b8440566a964d
zzzyoonnn <dwsrh03@gmail.com>
2025-05-15 18:22:48 +0900

    feat: 스프링 MVC의 컨트롤러 예외를 처리하는 @ControllerAdvice 실습 준비

---
+++ src/main/java/org/servlet2spring/servlet2spring/springSample/exception/CommonExceptionAdvice.java
@@ -0,0 +1,9 @@
+package org.servlet2spring.servlet2spring.springSample.exception;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@Log4j2
+@ControllerAdvice
+public class CommonExceptionAdvice {
+}
