f441338d8b4a9797f52c21a9baff68d81e920646
zzzyoonnn <dwsrh03@gmail.com>
2025-05-18 23:14:01 +0900

    test: MyBatis 매퍼 연동 확인용 TodoMapper 테스트 추가

---
+++ src/test/java/org/servlet2spring/mapper/TodoMapperTests.java
@@ -0,0 +1,23 @@
+package org.servlet2spring.mapper;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.servlet2spring.servlet2spring.todo.mapper.TodoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations="file:src/main/webapp/WEB-INF/root-context.xml")
+public class TodoMapperTests {
+
+  @Autowired(required = false)
+  private TodoMapper todoMapper;
+
+  @Test
+  public void testGetTime() {
+    log.info(todoMapper.getTime());
+  }
+}
