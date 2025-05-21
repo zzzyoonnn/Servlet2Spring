5eede2c503936a98c49cd129820275b3996c8868
zzzyoonnn <dwsrh03@gmail.com>
2025-05-22 00:29:07 +0900

    test: TodoService의 등록 기능 테스트 구현

---
+++ src/test/java/org/servlet2spring/service/TodoServiceTests.java
@@ -0,0 +1,31 @@
+package org.servlet2spring.service;
+
+import java.time.LocalDate;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.servlet2spring.todo.dto.TodoDTO;
+import org.servlet2spring.todo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
+public class TodoServiceTests {
+
+  @Autowired
+  private TodoService todoService;
+
+  @Test
+  public void testRegister() {
+    TodoDTO todoDTO = TodoDTO.builder()
+            .title("Register Test")
+            .dueDate(LocalDate.now())
+            .writer("user01")
+            .build();
+
+    todoService.register(todoDTO);
+  }
+}
