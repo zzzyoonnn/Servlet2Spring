89be22c446b369a3058fc643130326e2d587acfe
zzzyoonnn <dwsrh03@gmail.com>
2025-05-21 23:20:22 +0900

    feat: TodoVO를 파라미터로 입력받는 insert() 기능 구현

--- src/main/java/org/servlet2spring/todo/mapper/TodoMapper.java
+++ src/main/java/org/servlet2spring/todo/mapper/TodoMapper.java
@@ -1,7 +1,11 @@
 package org.servlet2spring.todo.mapper;
 
+import org.servlet2spring.todo.domain.TodoVO;
+
 public interface TodoMapper {
 
   String getTime();
+
+  void insert(TodoVO todoVO);
 
 }
