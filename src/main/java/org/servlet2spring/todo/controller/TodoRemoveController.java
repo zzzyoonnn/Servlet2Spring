d742c1b2fe827bd058f0b753f55c3c2c8e5a616e
zzzyoonnn <dwsrh03@gmail.com>
2025-05-28 16:22:21 +0900

    feat: TodoRemoveController에 remove() 메서드 초기 설계 (기능 미구현, 로그 확인용)

--- src/main/java/org/servlet2spring/todo/controller/TodoRemoveController.java
+++ src/main/java/org/servlet2spring/todo/controller/TodoRemoveController.java
@@ -6,27 +6,44 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.servlet2spring.todo.service.TodoService;
 import org.servlet2spring.todo.service.TodoService2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Log4j2
-@WebServlet(name = "todoRemoveController", value = "/todo/remove")
+@RequestMapping("/todo")
+@Controller
+//@WebServlet(name = "todoRemoveController", value = "/todo/remove")
 public class TodoRemoveController extends HttpServlet {
 
-  private TodoService2 todoService2 = TodoService2.INSTANCE;
+  //private TodoService2 todoService2 = TodoService2.INSTANCE;
 
-  @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    Long no = Long.parseLong(req.getParameter("no"));
+  @PostMapping("/remove")
+  public String remove(@RequestParam("no") Long no, RedirectAttributes redirectAttributes) {
+    log.info("-----remove-----");
     log.info("no: " + no);
 
-    try {
-      todoService2.remove(no);
-    } catch (Exception e) {
-      log.error(e.getMessage());
-      throw new ServletException("read error " + e);
-    }
-
-    resp.sendRedirect("/todo/list");
+    return "redirect:/todo/list";
   }
+//
+//  @Override
+//  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//    Long no = Long.parseLong(req.getParameter("no"));
+//    log.info("no: " + no);
+//
+//    try {
+//      todoService2.remove(no);
+//    } catch (Exception e) {
+//      log.error(e.getMessage());
+//      throw new ServletException("read error " + e);
+//    }
+//
+//    resp.sendRedirect("/todo/list");
+//  }
 }
