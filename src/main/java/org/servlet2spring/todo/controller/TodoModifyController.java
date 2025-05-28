412f30bd71df3db7885f1d119281c1508467bcb8
zzzyoonnn <dwsrh03@gmail.com>
2025-05-28 18:13:55 +0900

    feat: TodoModifyController에 Todo목록을 수정하는 modify() 기능 구현

--- src/main/java/org/servlet2spring/todo/controller/TodoModifyController.java
+++ src/main/java/org/servlet2spring/todo/controller/TodoModifyController.java
@@ -5,24 +5,47 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.servlet2spring.todo.dto.TodoDTO;
+import org.servlet2spring.todo.service.TodoService;
 import org.servlet2spring.todo.service.TodoService2;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Log4j2
+@Controller
 @RequestMapping("/todo")
+@RequiredArgsConstructor
 //@WebServlet(name = "todoModifyController", value = "/todo/modify")
 public class TodoModifyController extends HttpServlet {
 
-  private TodoService2 todoService2 = TodoService2.INSTANCE;
-  private final DateTimeFormatter DATEFORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+  private final TodoService todoService;
+//  private TodoService2 todoService2 = TodoService2.INSTANCE;
+//  private final DateTimeFormatter DATEFORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+  @PostMapping("/modify")
+  public String modify(@Valid TodoDTO todoDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    if (bindingResult.hasErrors()) {
+      log.info("has errors: {}", bindingResult.getAllErrors());
+
+      redirectAttributes.addAttribute("no", todoDTO.getNo());
+      return "redirect:/todo/modify";
+    }
+
+    log.info(todoDTO);
+    todoService.modify(todoDTO);
+
+    return "redirect:/todo/list";
+  }
 
 //  @Override
 //  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
