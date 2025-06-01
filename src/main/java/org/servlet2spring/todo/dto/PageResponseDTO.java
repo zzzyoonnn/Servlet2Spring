5351bd782c58b65230cdbba0f0f202f4433eb8a9
zzzyoonnn <dwsrh03@gmail.com>
2025-06-01 16:55:46 +0900

    feat: 페이지 번호 계산 로직 추가

--- src/main/java/org/servlet2spring/todo/dto/PageResponseDTO.java
+++ src/main/java/org/servlet2spring/todo/dto/PageResponseDTO.java
@@ -2,7 +2,11 @@ package org.servlet2spring.todo.dto;
 
 import java.util.List;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
 
+@Getter
+@ToString
 public class PageResponseDTO<E> {
   private int page;
   private int size;
@@ -20,12 +24,22 @@ public class PageResponseDTO<E> {
 
   private List<E> dtoList;
 
-  @Builder
+  @Builder(builderMethodName = "withAll")
   public PageResponseDTO(PageRequestDTO pageRequestDTO, List<E> dtoList, int total) {
     this.page = pageRequestDTO.getPage();
     this.size = pageRequestDTO.getSize();
 
     this.total = total;
     this.dtoList = dtoList;
+
+    // 마지막 페이지/시작 페이지 번호 계산
+    this.end = (int)(Math.ceil(this.page / 10.0)) * 10;
+    this.start = this.end - 9;
+    int last = (int)(Math.ceil((total / (double)size)));
+    this.end = end > last ? last : end;
+
+    // 이전(prev)/다음(next) 페이지 계산
+    this.prev = this.start > 1;
+    this.next = total > this.end * this.size;
   }
 }
