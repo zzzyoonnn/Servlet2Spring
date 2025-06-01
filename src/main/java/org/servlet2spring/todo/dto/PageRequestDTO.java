e060a595f419bbee46277d9f20f39ee0a77b62bd
zzzyoonnn <dwsrh03@gmail.com>
2025-06-01 15:27:44 +0900

    feat: 페이지 처리를 위한 PageRequestDTO 생성

---
+++ src/main/java/org/servlet2spring/todo/dto/PageRequestDTO.java
@@ -0,0 +1,31 @@
+package org.servlet2spring.todo.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageRequestDTO {
+
+  @Builder.Default
+  @Min(value = 1)
+  @Positive
+  private int page = 1;
+
+  @Builder.Default
+  @Min(value = 10)
+  @Max(value = 100)
+  @Positive
+  private int size = 10;
+
+  public int getSkip() {
+    return (page - 1) * size;
+  }
+}
