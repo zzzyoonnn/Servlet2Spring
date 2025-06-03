8e05c5ea95f5b33d627a247f8535d2044e80f19b
zzzyoonnn <dwsrh03@gmail.com>
2025-06-03 15:19:52 +0900

    feat: 페이지 이동에 필요한 링크들을 생성하는 getLink() 구현

--- src/main/java/org/servlet2spring/todo/dto/PageRequestDTO.java
+++ src/main/java/org/servlet2spring/todo/dto/PageRequestDTO.java
@@ -25,7 +25,20 @@ public class PageRequestDTO {
   @Positive
   private int size = 10;
 
+  private String link;
+
   public int getSkip() {
     return (page - 1) * size;
+  }
+
+  public String getLink() {
+    if (link == null) {
+      StringBuilder builder = new StringBuilder();
+      builder.append("page=" + this.page);
+      builder.append("&size=" + this.size);
+      link = builder.toString();
+    }
+
+    return link;
   }
 }
