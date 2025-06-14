96b2421af01704c0a800666e6bda64d0c1793ce3
zzzyoonnn <dwsrh03@gmail.com>
2025-06-15 00:00:13 +0900

    feat: Thymeleaf 출력 실습

--- src/main/java/org/servlet2spring/examples/springBootSample/SpringBootSampleController.java
+++ src/main/java/org/servlet2spring/examples/springBootSample/SpringBootSampleController.java
@@ -1,5 +1,7 @@
 package org.servlet2spring.examples.springBootSample;
 
+import java.util.Arrays;
+import java.util.List;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,5 +14,11 @@ public class SpringBootSampleController {
   public void helloSpringBoot(Model model) {
     log.info("hello");
     model.addAttribute("message", "Hello Spring Boot!");
+  }
+
+  @GetMapping("/ex/ex1")
+  public void ex1(Model model) {
+    List<String> list = Arrays.asList("A", "BB", "CCC");
+    model.addAttribute("list", list);
   }
 }
