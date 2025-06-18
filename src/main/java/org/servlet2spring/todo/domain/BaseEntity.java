0171ecabfa9f9a9a73d64f38bdf66c1a6227c43b
zzzyoonnn <dwsrh03@gmail.com>
2025-06-18 17:45:23 +0900

    feat: 등록일자 및 수정일자 자동 관리 기능을 위한 BaseEntity 생성

---
+++ src/main/java/org/servlet2spring/todo/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package org.servlet2spring.todo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+  @CreatedDate
+  @Column(name = "regdate", updatable = false)
+  private LocalDateTime regdate;
+
+  @LastModifiedDate
+  @Column(name = "moddate")
+  private LocalDateTime moddate;
+}
