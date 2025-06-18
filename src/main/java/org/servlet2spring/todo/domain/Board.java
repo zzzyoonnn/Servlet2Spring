b2adcda2d727321dd8e277968952018d9353a28e
zzzyoonnn <dwsrh03@gmail.com>
2025-06-18 17:51:29 +0900

    feat: BaseEntity 적용하여 AuditingEntityListener 활성화 기능 구현

--- src/main/java/org/servlet2spring/todo/domain/Board.java
+++ src/main/java/org/servlet2spring/todo/domain/Board.java
@@ -1,18 +1,35 @@
 package org.servlet2spring.todo.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
-public class Board {
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Board extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long bno;
+
+  @Column(length = 500, nullable = false)
   private String title;
+
+  @Column(length = 2000, nullable = false)
   private String content;
+
+  @Column(length = 50, nullable = false)
   private String writer;
 
 }
