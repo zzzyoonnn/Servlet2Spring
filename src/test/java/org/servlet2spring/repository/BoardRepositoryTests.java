1bd369ab03084062637af42fbe1acac4cc96c1ea
zzzyoonnn <dwsrh03@gmail.com>
2025-06-19 23:36:15 +0900

    test: Board 엔티티 페이징 기능 테스트 구현

--- src/test/java/org/servlet2spring/repository/BoardRepositoryTests.java
+++ src/test/java/org/servlet2spring/repository/BoardRepositoryTests.java
@@ -1,5 +1,6 @@
 package org.servlet2spring.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 import lombok.extern.log4j.Log4j2;
@@ -8,6 +9,10 @@ import org.servlet2spring.todo.domain.Board;
 import org.servlet2spring.todo.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @Log4j2
 @SpringBootTest
@@ -51,6 +56,20 @@ public class BoardRepositoryTests {
   public void testDelete() {
     Long bno = 99L;
     boardRepository.deleteById(bno);
+  }
+
+  @Test
+  public void testPaging() {
+    Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+    Page<Board> result = boardRepository.findAll(pageable);
+    log.info("total count: " + result.getTotalElements());
+    log.info("total pages: " + result.getTotalPages());
+    log.info("page number: " + result.getNumber());
+    log.info("page size: " + result.getSize());
+
+    List<Board> todoList = result.getContent();
+    todoList.forEach(board -> log.info(board));
   }
 
 }
