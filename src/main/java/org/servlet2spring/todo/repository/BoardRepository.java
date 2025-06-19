594d723ce540d75fe4d22d140def0b7405030513
zzzyoonnn <dwsrh03@gmail.com>
2025-06-19 23:48:31 +0900

    feat: @Query를 이용한 Board 키워드 검색 및 페이징 쿼리 구현

--- src/main/java/org/servlet2spring/todo/repository/BoardRepository.java
+++ src/main/java/org/servlet2spring/todo/repository/BoardRepository.java
@@ -1,7 +1,16 @@
 package org.servlet2spring.todo.repository;
 
+import java.awt.print.Pageable;
+import org.apache.ibatis.annotations.Param;
 import org.servlet2spring.todo.domain.Board;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+  //Page<Board> findByTitleContainingOrderByBnoDesc(String keyword, Pageable pageable);
+  @Query("select b from Board b where b.title like concat('%', :keyword, '%')")
+  Page<Board> findKeyword(@Param("keyword") String keyword, Pageable pageable);
+
 }
