0888165075e103419183c9265c36aebc6124ac5b
zzzyoonnn <dwsrh03@gmail.com>
2025-07-08 13:45:58 +0900

    feat: 게시판 등록 기능을 하는 register() 구현

---
+++ src/main/java/org/servlet2spring/todo/service/BoardService.java
@@ -0,0 +1,7 @@
+package org.servlet2spring.todo.service;
+
+import org.servlet2spring.todo.dto.BoardDTO;
+
+public interface BoardService {
+  Long register(BoardDTO boardDTO);
+}
---
+++ src/main/java/org/servlet2spring/todo/service/BoardServiceImpl.java
@@ -0,0 +1,26 @@
+package org.servlet2spring.todo.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.servlet2spring.todo.domain.Board;
+import org.servlet2spring.todo.dto.BoardDTO;
+import org.servlet2spring.todo.repository.BoardRepository;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+  private final ModelMapper modelMapper;
+  private final BoardRepository boardRepository;
+
+  @Override
+  public Long register(BoardDTO boardDTO) {
+    Board board = modelMapper.map(boardDTO, Board.class);
+    Long bno = boardRepository.save(board).getBno();
+    return bno;
+  }
+}
