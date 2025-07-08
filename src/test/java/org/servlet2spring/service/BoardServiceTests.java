52a77d5f3ce900509fda7746714ba0d7c5958e90
zzzyoonnn <dwsrh03@gmail.com>
2025-07-08 14:03:12 +0900

    test: 게시판 등록 기능을 하는 register() 테스트 구현

--- src/test/java/org/servlet2spring/service/BoardServiceTests.java
+++ src/test/java/org/servlet2spring/service/BoardServiceTests.java
@@ -2,6 +2,7 @@ package org.servlet2spring.service;
 
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
+import org.servlet2spring.todo.dto.BoardDTO;
 import org.servlet2spring.todo.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,5 +17,15 @@ public class BoardServiceTests {
   @Test
   public void testRegister() {
     log.info(boardService.getClass().getName());
+
+    BoardDTO boardDTO = BoardDTO.builder()
+            .title("test register")
+            .content("test content")
+            .writer("user00")
+            .build();
+
+    Long bno = boardService.register(boardDTO);
+
+    log.info("bno: " + bno);
   }
 }
