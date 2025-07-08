1034ffe95673d971ef0881b7085c566d4aa1ef1f
zzzyoonnn <dwsrh03@gmail.com>
2025-07-08 16:38:00 +0900

    test: 목록/검색 페이징 처리된 목록을 조회하는 list() 테스트 구현

--- src/test/java/org/servlet2spring/service/BoardServiceTests.java
+++ src/test/java/org/servlet2spring/service/BoardServiceTests.java
@@ -3,6 +3,8 @@ package org.servlet2spring.service;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.servlet2spring.todo.dto.BoardDTO;
+import org.servlet2spring.todo.dto.PageRequestDTO;
+import org.servlet2spring.todo.dto.PageResponseDTO;
 import org.servlet2spring.todo.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,15 +31,29 @@ public class BoardServiceTests {
     log.info("bno: " + bno);
   }
 
-@Test
-public void testModify() {
-  // 변경에 필요한 데이터만 작성
-  BoardDTO boardDTO = BoardDTO.builder()
-          .bno(101L)
-          .title("test modify")
-          .content("test test")
-          .build();
+  @Test
+  public void testModify() {
+    // 변경에 필요한 데이터만 작성
+    BoardDTO boardDTO = BoardDTO.builder()
+            .bno(101L)
+            .title("test modify")
+            .content("test test")
+            .build();
 
-  boardService.modify(boardDTO);
-}
+    boardService.modify(boardDTO);
+  }
+
+  @Test
+  public void testList() {
+    PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+            .type("tcw")
+            .keyword("1")
+            .page(1)
+            .size(10)
+            .build();
+
+    PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+
+    log.info(responseDTO);
+  }
 }
