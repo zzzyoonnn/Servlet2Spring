be024d4131da4916257c63091c734c2f8dc0fa57
zzzyoonnn <dwsrh03@gmail.com>
2025-07-09 22:49:20 +0900

    feat: 게시물 화면 구성을 위한 기본 코드 추가

---
+++ src/main/java/org/servlet2spring/todo/controller/BoardController.java
@@ -0,0 +1,23 @@
+package org.servlet2spring.todo.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.servlet2spring.todo.dto.PageRequestDTO;
+import org.servlet2spring.todo.service.BoardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j2
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+  private final BoardService boardService;
+
+  @GetMapping("/list")
+  public void list(PageRequestDTO pageRequestDTO, Model model) {
+
+  }
+}
