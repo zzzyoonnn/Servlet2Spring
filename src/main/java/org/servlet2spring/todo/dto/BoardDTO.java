c850357b418aea9a79bfa994d61d05cda0bcffeb
zzzyoonnn <dwsrh03@gmail.com>
2025-07-13 17:19:57 +0900

    feat: 유효성 검증 애너테이션 추가

--- src/main/java/org/servlet2spring/todo/dto/BoardDTO.java
+++ src/main/java/org/servlet2spring/todo/dto/BoardDTO.java
@@ -1,5 +1,7 @@
 package org.servlet2spring.todo.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,9 +15,17 @@ import lombok.NoArgsConstructor;
 public class BoardDTO {
 
   private Long bno;
+
+  @NotEmpty
+  @Size(min = 3, max = 100)
   private String title;
+
+  @NotEmpty
   private String content;
+
+  @NotEmpty
   private String writer;
+
   private LocalDateTime regDate;
   private LocalDateTime modDate;
 
