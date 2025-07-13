371a241f6c6be6e7bef0fb63438e9f1ced89a213
zzzyoonnn <dwsrh03@gmail.com>
2025-07-13 17:25:05 +0900

    feat: 게시글을 등록 처리용 POST 메서드 추가

--- src/main/java/org/servlet2spring/todo/controller/BoardController.java
+++ src/main/java/org/servlet2spring/todo/controller/BoardController.java
@@ -1,5 +1,6 @@
 package org.servlet2spring.todo.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.servlet2spring.todo.dto.BoardDTO;
@@ -8,8 +9,11 @@ import org.servlet2spring.todo.dto.PageResponseDTO;
 import org.servlet2spring.todo.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Log4j2
 @Controller
@@ -18,10 +22,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BoardController {
   private final BoardService boardService;
 
+  // 목록 기능
   @GetMapping("/list")
   public void list(PageRequestDTO pageRequestDTO, Model model) {
     PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
     log.info(responseDTO);
     model.addAttribute("responseDTO", responseDTO);
+  }
+
+  // 등록 기능
+  @GetMapping("/register")
+  public void registerGet(){
+
+  }
+
+  @PostMapping("/register")
+  public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    log.info("board POST register");
+
+    if (bindingResult.hasErrors()) {
+      log.info("has errors");
+      redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+      return "redirect:/board/register";
+    }
+
+    log.info(boardDTO);
+
+    Long bno = boardService.register(boardDTO);
+    redirectAttributes.addFlashAttribute("result", bno);
+
+    return "redirect:/board/list";
   }
 }
