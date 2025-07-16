940d20e3bb2dd8a6d48799a238dd2670262abcfa
zzzyoonnn <dwsrh03@gmail.com>
2025-07-16 18:26:49 +0900

    fix: Swagger 인식을 위해 @RestController로 수정

--- src/main/java/org/servlet2spring/todo/controller/BoardController.java
+++ src/main/java/org/servlet2spring/todo/controller/BoardController.java
@@ -13,10 +13,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Log4j2
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
