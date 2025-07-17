f097a45d7f7e2684eb181c4388516a8507458df4
zzzyoonnn <dwsrh03@gmail.com>
2025-07-17 18:26:04 +0900

    feat: 댓글 등록 요청에 대한 유효성 검증 기능 구현

--- src/main/java/org/servlet2spring/todo/controller/ReplyController.java
+++ src/main/java/org/servlet2spring/todo/controller/ReplyController.java
@@ -1,11 +1,14 @@
 package org.servlet2spring.todo.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import java.util.Map;
 import lombok.extern.log4j.Log4j2;
 import org.servlet2spring.todo.dto.ReplyDTO;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +21,14 @@ public class ReplyController {
 
   @Operation(summary = "Replies POST", description = "POST 방식으로 댓글 등록")
   @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Map<String, Long>> register(@RequestBody ReplyDTO replyDTO) {
+  public ResponseEntity<Map<String, Long>> register(@Valid @RequestBody ReplyDTO replyDTO, BindingResult bindingResult) throws BindException {
 
     log.info(replyDTO.toString());
+
+    if (bindingResult.hasErrors()) {
+      throw new BindException(bindingResult);
+    }
+
     Map<String, Long> resultMap = Map.of("rno", 111L);
     return ResponseEntity.ok(resultMap);
   }
