5f6104cab6625b0efb80228113ee2ab8f07742d7
zzzyoonnn <dwsrh03@gmail.com>
2025-07-17 18:18:23 +0900

    feat: 유효성 검증 예외 처리를 위한 CustomRestAdvice 생성

---
+++ src/main/java/org/servlet2spring/todo/controller/advice/CustomRestAdvice.java
@@ -0,0 +1,34 @@
+package org.servlet2spring.todo.controller.advice;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Log4j2
+@RestControllerAdvice
+public class CustomRestAdvice {
+
+  @ExceptionHandler(BindException.class)
+  @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+  public ResponseEntity<Map<String, String>> handleBindException(BindException e) {
+    log.error(e);
+    Map<String, String> errorMap = new HashMap<>();
+
+    if (e.hasErrors()) {
+      BindingResult bindingResult = e.getBindingResult();
+
+      bindingResult.getFieldErrors().forEach(fieldError -> {
+        errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+      });
+
+    }
+    return ResponseEntity.badRequest().body(errorMap);
+  }
+}
