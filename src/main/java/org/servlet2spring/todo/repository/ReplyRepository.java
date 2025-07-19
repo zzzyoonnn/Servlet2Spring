c8eef1f97e84557717f2ff5a8c9865ed20408bd0
zzzyoonnn <dwsrh03@gmail.com>
2025-07-19 14:40:54 +0900

    feat: 게시글 번호 기준으로 댓글 목록 페이징 조회하는 listOfBoard() 구현

--- src/main/java/org/servlet2spring/todo/repository/ReplyRepository.java
+++ src/main/java/org/servlet2spring/todo/repository/ReplyRepository.java
@@ -1,7 +1,14 @@
 package org.servlet2spring.todo.repository;
 
+import org.apache.ibatis.annotations.Param;
 import org.servlet2spring.todo.domain.Reply;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+  @Query("select r from Reply r where r.board.bno = :bno")
+  Page<Reply> listOfBoard(@Param("bno") Long bno, Pageable pageable);
 }
