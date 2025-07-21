76f7bc3e20f8147742c43d62218ec161894c359f
zzzyoonnn <dwsrh03@gmail.com>
2025-07-21 14:16:29 +0900

    feat: 게시글별 댓글 수를 조회하는 searchWithReplyCount() 구현

--- src/main/java/org/servlet2spring/todo/search/BoardSearch.java
+++ src/main/java/org/servlet2spring/todo/search/BoardSearch.java
@@ -1,5 +1,6 @@
 package org.servlet2spring.todo.search;
 
+import org.servlet2spring.todo.dto.BoardListReplyCountDTO;
 import org.springframework.data.domain.Pageable;
 import org.servlet2spring.todo.domain.Board;
 import org.springframework.data.domain.Page;
@@ -8,4 +9,6 @@ public interface BoardSearch {
   Page<Board> search1(Pageable pageable);
 
   Page<Board> searchAll(String[] types, String keyword, Pageable pageable);
+
+  Page<BoardListReplyCountDTO> searchWithReplyCount(String[] types, String keyword, Pageable pageable);
 }
--- src/main/java/org/servlet2spring/todo/search/BoardSearchImpl.java
+++ src/main/java/org/servlet2spring/todo/search/BoardSearchImpl.java
@@ -1,11 +1,14 @@
 package org.servlet2spring.todo.search;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLQuery;
 import java.util.List;
 import org.hibernate.annotations.DialectOverride.SQLRestriction;
 import org.servlet2spring.todo.domain.Board;
 import org.servlet2spring.todo.domain.QBoard;
+import org.servlet2spring.todo.domain.QReply;
+import org.servlet2spring.todo.dto.BoardListReplyCountDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -70,5 +73,52 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
     long count = query.fetchCount();
 
     return new PageImpl<>(list, pageable, count);
+  }
+
+  @Override
+  public Page<BoardListReplyCountDTO> searchWithReplyCount(String[] types, String keyword, Pageable pageable) {
+    QBoard board = QBoard.board;
+    QReply reply = QReply.reply;
+
+    JPQLQuery<Board> query = from(board);
+    query.leftJoin(reply).on(reply.board.eq(board));
+
+    query.groupBy(board);
+
+    if ((types != null && types.length > 0) && keyword != null) {
+      BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+      for (String type : types) {
+        switch (type) {
+          case "t":
+            booleanBuilder.or(board.title.contains(keyword));
+            break;
+          case "c":
+            booleanBuilder.or(board.content.contains(keyword));
+            break;
+          case "w":
+            booleanBuilder.or(board.writer.contains(keyword));
+            break;
+        }
+      } // end for
+      query.where(booleanBuilder);
+    }
+
+    // bno > 0
+    query.where(board.bno.gt(0L));
+
+    JPQLQuery<BoardListReplyCountDTO> dtoQuery = query.select(Projections.bean(BoardListReplyCountDTO.class,
+            board.bno,
+            board.title,
+            board.writer,
+            board.regdate.as("regDate"),
+            reply.count().as("replyCount")
+    ));
+
+    this.getQuerydsl().applyPagination(pageable, dtoQuery);
+    List<BoardListReplyCountDTO> dtoList = dtoQuery.fetch();
+    long count = dtoQuery.fetchCount();
+
+    return new PageImpl<>(dtoList, pageable, count);
   }
 }
