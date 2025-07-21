5d392ad5fe4066c961fc8343a82ad87d71922826
zzzyoonnn <dwsrh03@gmail.com>
2025-07-21 14:30:30 +0900

    test: 게시글별 댓글 수를 조회하는 searchWithReplyCount() 테스트 구현

--- src/test/java/org/servlet2spring/repository/BoardRepositoryTests.java
+++ src/test/java/org/servlet2spring/repository/BoardRepositoryTests.java
@@ -6,6 +6,7 @@ import java.util.stream.IntStream;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.servlet2spring.todo.domain.Board;
+import org.servlet2spring.todo.dto.BoardListReplyCountDTO;
 import org.servlet2spring.todo.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -88,6 +89,30 @@ public class BoardRepositoryTests {
 
     Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
     Page<Board> result = boardRepository.searchAll(types, keyword, pageable);
+
+    // total pages
+    log.info(result.getTotalPages());
+
+    // page size
+    log.info(result.getSize());
+
+    // page number
+    log.info(result.getNumber());
+
+    // prev next
+    log.info(result.hasPrevious() + ": " + result.hasNext());
+
+    result.getContent().forEach(board -> log.info(board));
+  }
+
+  @Test
+  public void testSearchReplyCount() {
+    String[] types = {"t", "c", "w"};
+
+    String keyword = "1";
+
+    Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+    Page<BoardListReplyCountDTO> result = boardRepository.searchWithReplyCount(types, keyword, pageable);
 
     // total pages
     log.info(result.getTotalPages());
