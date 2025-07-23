d0cccf27f7526d8d70f1d99b167f07d9c0aae220
zzzyoonnn <dwsrh03@gmail.com>
2025-07-23 23:24:36 +0900

    feat: 컨트롤러에 댓글 등록 API 구현

--- src/main/java/org/servlet2spring/todo/controller/ReplyController.java
+++ src/main/java/org/servlet2spring/todo/controller/ReplyController.java
@@ -2,11 +2,13 @@ package org.servlet2spring.todo.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import java.util.HashMap;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.servlet2spring.todo.dto.ReplyDTO;
+import org.servlet2spring.todo.service.ReplyService;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
@@ -18,21 +20,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Log4j2
 @Controller
 @RequestMapping("/replies")
+@RequiredArgsConstructor
 public class ReplyController {
+
+  private final ReplyService replyService;
 
   @ResponseBody
   @Operation(summary = "Replies POST", description = "POST 방식으로 댓글 등록")
   @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Map<String, Long>> register(@Valid @RequestBody ReplyDTO replyDTO, BindingResult bindingResult) throws BindException {
-
-    log.info(replyDTO.toString());
+  public Map<String, Long> register(@Valid @RequestBody ReplyDTO replyDTO, BindingResult bindingResult) throws BindException {
+    log.info(replyDTO);
 
     if (bindingResult.hasErrors()) {
       throw new BindException(bindingResult);
     }
 
-    Map<String, Long> resultMap = Map.of("rno", 111L);
-    return ResponseEntity.ok(resultMap);
-  }
+    Map<String, Long> resultMap = new HashMap<>();
+    Long rno = replyService.register(replyDTO);
+    resultMap.put("rno", rno);
 
+    return resultMap;
+  }
 }
