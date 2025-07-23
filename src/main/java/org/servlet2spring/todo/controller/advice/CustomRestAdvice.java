7a7d2252ad5e2fc8fcd1bce38370e5d67688e975
zzzyoonnn <dwsrh03@gmail.com>
2025-07-23 23:49:38 +0900

    feat: 데이터 무결성 오류 대응하는 handleFKException() 구현

--- src/main/java/org/servlet2spring/todo/controller/advice/CustomRestAdvice.java
+++ src/main/java/org/servlet2spring/todo/controller/advice/CustomRestAdvice.java
@@ -3,10 +3,12 @@ package org.servlet2spring.todo.controller.advice;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -29,6 +31,18 @@ public class CustomRestAdvice {
       });
 
     }
+    return ResponseEntity.badRequest().body(errorMap);
+  }
+
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+  public ResponseEntity<Map<String, String>> handleFKException(Exception e) {
+    log.error(e);
+
+    Map<String, String> errorMap = new HashMap<>();
+    errorMap.put("time", ""+System.currentTimeMillis());
+    errorMap.put("message", "constraint fails");
+
     return ResponseEntity.badRequest().body(errorMap);
   }
 }
