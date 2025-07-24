90129e7c128d59f575c890b3512de984b8ef485e
zzzyoonnn <dwsrh03@gmail.com>
2025-07-24 15:54:45 +0900

    feat: 컨트롤러에 특정 댓글 조회 API 구현

--- src/main/java/org/servlet2spring/todo/controller/ReplyController.java
+++ src/main/java/org/servlet2spring/todo/controller/ReplyController.java
@@ -6,12 +6,18 @@ import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.servlet2spring.todo.domain.Reply;
+import org.servlet2spring.todo.dto.PageRequestDTO;
+import org.servlet2spring.todo.dto.PageResponseDTO;
 import org.servlet2spring.todo.dto.ReplyDTO;
 import org.servlet2spring.todo.service.ReplyService;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +31,7 @@ public class ReplyController {
 
   private final ReplyService replyService;
 
+  // 댓글 등록
   @ResponseBody
   @Operation(summary = "Replies POST", description = "POST 방식으로 댓글 등록")
   @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -40,5 +47,15 @@ public class ReplyController {
     resultMap.put("rno", rno);
 
     return resultMap;
+  }
+
+  // 특정 댓글 조회
+  @Operation(summary = "Read Reply", description = "GET 방식으로 특정 댓글 조회")
+  @GetMapping("/{rno}")
+  @ResponseBody
+  public ReplyDTO getReplyDTO(@PathVariable("rno") Long rno) {
+    ReplyDTO replyDTO = replyService.read(rno);
+
+    return replyDTO;
   }
 }
