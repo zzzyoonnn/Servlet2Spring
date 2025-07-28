0abb3107f8fd985da834eb6fea92c50841a5e9b0
zzzyoonnn <dwsrh03@gmail.com>
2025-07-28 16:50:08 +0900

    feat: 최신 댓글이 보이도록 기능 추가

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -7,5 +7,11 @@ async function get1(bno) {
 async function getList({bno, page, size, goLast}) {
     const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}});
 
+    if (goLast) {
+        const total = result.data.total;
+        const lastPage = parseInt(Math.ceil(total / size));
+        
+        return getList({bno:bno, page:lastPage, size:size})
+    }
     return result.data;
 }
