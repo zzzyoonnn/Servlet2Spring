81bfec1c649b30be882acda65f85be29f86289fd
zzzyoonnn <dwsrh03@gmail.com>
2025-08-01 14:34:42 +0900

    feat: 파일 업로드를 위한 UploadFileDTO 생성

---
+++ src/main/java/org/servlet2spring/todo/dto/upload/UploadFileDTO.java
@@ -0,0 +1,16 @@
+package org.servlet2spring.todo.dto.upload;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+public class UploadFileDTO {
+
+  @Schema(type = "string", format = "binary")
+  private List<MultipartFile> files;
+
+}
