33defcfe713874b089f95426f7f179b08dc0e54f
zzzyoonnn <dwsrh03@gmail.com>
2025-08-01 15:36:35 +0900

    feat: 파일 업로드 결과를 List에 담아 반환하는 기능 추가

--- src/main/java/org/servlet2spring/todo/controller/UpDownController.java
+++ src/main/java/org/servlet2spring/todo/controller/UpDownController.java
@@ -5,9 +5,11 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import lombok.extern.log4j.Log4j2;
+import org.servlet2spring.todo.dto.upload.UploadResultDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,15 +26,17 @@ public class UpDownController {
 
   @Operation(summary = "Upload POST", description = "POST 방식으로 파일 등록")
   @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-  public String upload(@RequestPart("files") List<MultipartFile> files) {
+  public List upload(@RequestPart("files") List<MultipartFile> files) {
     log.info("Uploaded files: {}", files);
 
     if (files != null) {
+      final List<UploadResultDTO> resultList = new ArrayList<>();
       files.forEach(file -> {
-      String fileName = file.getOriginalFilename();
-      String uuid = UUID.randomUUID().toString();
 
-      Path savePath = Paths.get(uploadPath, uuid + "" + fileName);
+        String fileName = file.getOriginalFilename();
+        String uuid = UUID.randomUUID().toString();
+
+        Path savePath = Paths.get(uploadPath, uuid + "" + fileName);
 
         try {
           file.transferTo(new File(String.valueOf(savePath))); // 실제 파일 저장
@@ -41,7 +45,14 @@ public class UpDownController {
         } catch (IOException e) {
           e.printStackTrace();
         }
+
+        resultList.add(UploadResultDTO.builder()
+                .uuid(uuid)
+                .fileName(fileName)
+                .build());
       });
+
+      return resultList;
     }
 
     return null;
