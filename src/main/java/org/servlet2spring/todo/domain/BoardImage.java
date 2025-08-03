d952b5050a9262e42771ae1048be7bfadbe58903
zzzyoonnn <dwsrh03@gmail.com>
2025-08-03 23:58:45 +0900

    feat: 첨부 파일을 의미하는 BoardImage 엔티티 클래스 생성

---
+++ src/main/java/org/servlet2spring/todo/domain/BoardImage.java
@@ -0,0 +1,38 @@
+package org.servlet2spring.todo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "board")
+public class BoardImage implements Comparable<BoardImage> {
+
+  @Id
+  private String uuid;
+
+  private String fileName;
+  private int ord;
+
+  @ManyToOne
+  private Board board;
+
+  @Override
+  public int compareTo(BoardImage o) {
+    return ord - o.ord;
+  }
+
+  public void changeBoard(Board board) {
+    this.board = board;
+  }
+}
