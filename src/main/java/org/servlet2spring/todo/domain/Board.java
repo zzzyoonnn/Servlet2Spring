157ba8db8436fc089a79ee60f08b95704da30608
zzzyoonnn <dwsrh03@gmail.com>
2025-08-04 00:01:14 +0900

    feat: Board에 BoardImage에 대한 참조 추가

--- src/main/java/org/servlet2spring/todo/domain/Board.java
+++ src/main/java/org/servlet2spring/todo/domain/Board.java
@@ -5,6 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,7 +19,7 @@ import lombok.ToString;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@ToString(exclude = "imageSet")
 public class Board extends BaseEntity {
 
   @Id
@@ -32,9 +35,13 @@ public class Board extends BaseEntity {
   @Column(length = 50, nullable = false)
   private String writer;
 
-
   public void change(String title, String content) {
     this.title = title;
     this.content = content;
   }
+
+  @OneToMany
+  @Builder.Default
+  private Set<BoardImage> imageSet = new HashSet<>();
+
 }
