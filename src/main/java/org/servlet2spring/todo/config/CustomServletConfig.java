a7945dbe76e37a0be01ecb3f04854b5aef206830
zzzyoonnn <dwsrh03@gmail.com>
2025-08-19 00:08:48 +0900

    chore: MVC 설정 준비

---
+++ src/main/java/org/servlet2spring/todo/config/CustomServletConfig.java
@@ -0,0 +1,9 @@
+package org.servlet2spring.todo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+public class CustomServletConfig {
+}
