38bcdd8b69a7e8393896abc1a7d85f16417f57b8
zzzyoonnn <dwsrh03@gmail.com>
2025-08-18 23:44:00 +0900

    chore: CSRF 토큰 비활성화 및 Stateless 설정 적용

---
+++ src/main/java/org/servlet2spring/todo/config/CustomSecurityConfig.java
@@ -0,0 +1,59 @@
+package org.servlet2spring.todo.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Log4j2
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class CustomSecurityConfig {
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    log.info("---------- web config ----------");
+
+    return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+    log.info("---------- filter chain ---------");
+
+    http
+            .csrf(csrf -> csrf.disable()) // Lambda DSL 사용
+            .sessionManagement(session ->
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .authorizeHttpRequests(auth ->
+                    auth.anyRequest().authenticated()
+            );
+
+    return http.build();
+  }
+
+  @Bean
+  public MethodSecurityExpressionHandler methodSecurityExpressionHandler() {
+    return new DefaultMethodSecurityExpressionHandler();
+  }
+}
