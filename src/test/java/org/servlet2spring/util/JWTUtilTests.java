2de03791954f7c79f10c1ee5267dfb0d0146de6f
zzzyoonnn <dwsrh03@gmail.com>
2025-08-28 18:13:33 +0900

    test: validateToken 반환값 검증 테스트 구현

--- src/test/java/org/servlet2spring/util/JWTUtilTests.java
+++ src/test/java/org/servlet2spring/util/JWTUtilTests.java
@@ -1,5 +1,6 @@
 package org.servlet2spring.util;
 
+import java.util.HashMap;
 import java.util.Map;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
@@ -31,5 +32,16 @@ public class JWTUtilTests {
     Map<String, Object> claim = jwtUtil.validateToken(jwtStr);
 
     log.info(claim.toString());
+  }
+
+  @Test
+  public void testAll() {
+    String jwtStr = jwtUtil.generateToken(Map.of("mid", "AAAA", "email", "aaa@bbb.com"), 1);
+
+    log.info(jwtStr);
+
+    Map<String, Object> claim = jwtUtil.validateToken(jwtStr);
+    log.info("MID: " + claim.get("mid"));
+    log.info("EMAIL: " + claim.get("email"));
   }
 }
