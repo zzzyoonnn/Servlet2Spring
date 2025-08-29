bbe0bee89d7e5f848148a940efe441a4d54dee93
zzzyoonnn <dwsrh03@gmail.com>
2025-08-29 16:16:47 +0900

    feat: TokenCheckFilter에 JWT 토큰 추출 및 검증 기능 구현

--- src/main/java/org/servlet2spring/todo/security/filter/TokenCheckFilter.java
+++ src/main/java/org/servlet2spring/todo/security/filter/TokenCheckFilter.java
@@ -1,12 +1,18 @@
 package org.servlet2spring.todo.security.filter;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.servlet2spring.todo.security.exception.AccessTokenException;
+import org.servlet2spring.todo.security.exception.AccessTokenException.TOKEN_ERROR;
 import org.servlet2spring.todo.util.JWTUtil;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,8 +22,40 @@ public class TokenCheckFilter extends OncePerRequestFilter {
 
   private final JWTUtil jwtUtil;
 
+  private Map<String, Object> validateAccessToken(HttpServletRequest request) throws AccessTokenException {
+    String headerStr = request.getHeader("Authorization");
+
+    if (headerStr == null || headerStr.length() < 8) {
+      throw new AccessTokenException(TOKEN_ERROR.UNACCEPT);
+    }
+
+    // Bearer 생략
+    String tokenType = headerStr.substring(0, 6);
+    String tokenStr = headerStr.substring(7);
+
+    if (tokenType.equalsIgnoreCase("Bearer") == false) {
+      throw new AccessTokenException(TOKEN_ERROR.BADTYPE);
+    }
+
+    try {
+      Map<String, Object> values = jwtUtil.validateToken(tokenStr);
+
+      return values;
+    } catch (MalformedJwtException e) {
+      log.error("---------- MalformedJwtException ----------");
+      throw new AccessTokenException(TOKEN_ERROR.MALFORM);
+    } catch (SignatureException e) {
+      log.error("---------- SignatureException ----------");
+      throw new AccessTokenException(TOKEN_ERROR.BADSIGN);
+    } catch (ExpiredJwtException e) {
+      log.error("---------- ExpiredJwtException ----------");
+      throw new AccessTokenException(TOKEN_ERROR.EXPIRED);
+    }
+  }
+
   @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+          throws ServletException, IOException {
     String path = request.getRequestURI();
 
     if (!path.startsWith("/")) {
@@ -28,6 +66,11 @@ public class TokenCheckFilter extends OncePerRequestFilter {
     log.info("---------- Token Check Filter ----------");
     log.info("JWTUtil: " + jwtUtil.toString());
 
-    filterChain.doFilter(request, response);
+    try {
+      validateAccessToken(request);
+      filterChain.doFilter(request, response);
+    } catch (AccessTokenException e) {
+      e.sendResponseError(response);
+    }
   }
 }
