ace8721d1e19e4019ade88f9f8f29018fffa0513
zzzyoonnn <dwsrh03@gmail.com>
2025-09-17 13:15:48 +0900

    feat: UserDetails 기반 간단한 로그인 기능 구현

--- src/main/java/org/servlet2spring/todo/config/CustomSecurityConfig.java
+++ src/main/java/org/servlet2spring/todo/config/CustomSecurityConfig.java
@@ -1,5 +1,7 @@
 package org.servlet2spring.todo.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -7,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
@@ -15,8 +19,15 @@ import org.springframework.security.web.SecurityFilterChain;
 public class CustomSecurityConfig {
 
   @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     log.info("--------------- configure ---------------");
+
+    http.formLogin(withDefaults());
 
     return http.build();
   }
