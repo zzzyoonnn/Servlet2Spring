d182242d75e4be1fee22248650f9bfe12fc19a32
zzzyoonnn <dwsrh03@gmail.com>
2025-09-21 00:35:07 +0900

    feat: 자동로그인 기능 구현

--- src/main/java/org/servlet2spring/todo/config/CustomSecurityConfig.java
+++ src/main/java/org/servlet2spring/todo/config/CustomSecurityConfig.java
@@ -2,8 +2,10 @@ package org.servlet2spring.todo.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.servlet2spring.todo.security.CustomUserDetailsService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +15,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @Log4j2
 @Configuration
 @RequiredArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class CustomSecurityConfig {
+
+  private final DataSource dataSource;
+  private final CustomUserDetailsService userDetailsService;
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -29,10 +36,18 @@ public class CustomSecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     log.info("--------------- configure ---------------");
 
-    //http.formLogin(withDefaults()).loginPage("/member/login");
+    // 커스텀 로그인 페이지
     http.formLogin(form -> form.loginPage("/member/login"));
 
+    // CSRF 토큰 비활성화
     http.csrf(config -> config.disable());
+
+    http.rememberMe(rememberMe -> rememberMe
+            .key("1234")
+            .tokenRepository(persistentTokenRepository())
+            .userDetailsService(userDetailsService)
+            .tokenValiditySeconds(60 * 60 * 24 * 30)
+    );
 
     return http.build();
   }
@@ -44,5 +59,12 @@ public class CustomSecurityConfig {
     log.info("--------------- web configure ---------------");
 
     return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+  }
+
+  @Bean
+  public PersistentTokenRepository persistentTokenRepository() {
+    JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+    tokenRepository.setDataSource(dataSource);
+    return tokenRepository;
   }
 }
