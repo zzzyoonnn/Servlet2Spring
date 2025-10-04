3518410116f81b13f42e5face9227f7dede5340b
zzzyoonnn <dwsrh03@gmail.com>
2025-10-04 12:07:10 +0900

    feat: 소셜 로그인 사용자 데이터 추출 기능 구현

--- src/main/java/org/servlet2spring/todo/security/CustomOAuth2UserService.java
+++ src/main/java/org/servlet2spring/todo/security/CustomOAuth2UserService.java
@@ -1,7 +1,10 @@
 package org.servlet2spring.todo.security;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -18,6 +21,45 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     log.info("userRequest...");
     log.info("{}", userRequest);
 
-    return super.loadUser(userRequest);
+    log.info("oauth user --------------------");
+
+    ClientRegistration clientRegistration = userRequest.getClientRegistration();
+    String clientName = clientRegistration.getClientName();
+
+    log.info("Name: " + clientName);
+
+    OAuth2User oAuth2User = super.loadUser(userRequest);
+    Map<String, Object> paramMap = oAuth2User.getAttributes();
+
+    paramMap.forEach((key, value) -> {
+      log.info("--------------------");
+      log.info("key: " + key + ", value: " + value);
+    });
+
+//    String email = null;
+//
+//    switch (clientName) {
+//      case "kakao":
+//        email = getKakaoEmail(paramMap);
+//        break;
+//    }
+//
+//    log.info(email);
+
+    return oAuth2User;
   }
+
+//  private String getKakaoEmail(Map<String, Object> paramMap) {
+//    log.info("Kakao --------------------");
+//
+//    Object value = paramMap.get("kakao_account");
+//
+//    log.info(value);
+//
+//    LinkedHashMap accountMap = (LinkedHashMap) value;
+//    String email = (String) accountMap.get("email");
+//    log.info("email: " + email);
+//
+//    return email;
+//  }
 }
